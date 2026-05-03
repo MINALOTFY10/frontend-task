4dae851de9e307aa633a17f386d84494f100939c
MINALOTFY10 <menalotfy665@gmail.com>
2026-05-03 13:42:53 +0300

    feat: added theme toggling feature using global theme context

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import "./App.css";
 import { Button, Card } from "react-bootstrap";
+import { useTheme } from "./context/theme-context";
 
 function App() {
+  const { theme, toggleTheme } = useTheme();
+
   return (
     <div>
       <section id="next-steps">
         <Card className="p-3">
           <Card.Body>
             <Card.Title>Frontend Task</Card.Title>
-            <Button variant="primary">Click</Button>
+            <Button variant={theme === "light" ? "dark" : "light"} onClick={toggleTheme}>
+              {theme === "light" ? "dark" : "light"}
+            </Button>
           </Card.Body>
         </Card>
       </section>
